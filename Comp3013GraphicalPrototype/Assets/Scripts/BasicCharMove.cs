7be924adc4d2e08564612c0d8e6a02ce05fe9abe
PSC <philakers@live.co.uk>
2021-11-18 12:58:25 +0000

    stuff

---
+++ Comp3013GraphicalPrototype/Assets/Scripts/BasicCharMove.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicCharMove : MonoBehaviour
+{
+    private Vector3 tempPos;
+    public GameObject character;
+    public float movementModifier;
+    public float jumpModifier;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Right"))
+        {
+
+            tempPos = character.transform.position;
+
+            Vector3 newPos;
+            newPos.x = transform.position.x + movementModifier;
+            newPos.y = transform.position.y;
+            newPos.z = transform.position.z;
+
+            character.transform.position = Vector2.Lerp(transform.position, newPos, 1f);
+        }
+        if (Input.GetButton("Left"))
+        {
+
+            tempPos = character.transform.position;
+
+            Vector3 newPos;
+            newPos.x = transform.position.x - movementModifier;
+            newPos.y = transform.position.y;
+            newPos.z = transform.position.z;
+
+            character.transform.position = Vector2.Lerp(transform.position, newPos, 1f);
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+
+            tempPos = character.transform.position;
+            if (tempPos.y <  -2.8)
+            {
+                Vector3 newPos;
+                newPos.x = transform.position.x;
+                newPos.y = transform.position.y + jumpModifier;
+                newPos.z = transform.position.z;
+
+                character.transform.position = Vector2.Lerp(transform.position, newPos, 1f);
+            }
+            //jumpCode
+        }
+    }
+}
---
+++ Comp3013GraphicalPrototype/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public Button exitBtn;
+    public Button playBtn;
+    public Canvas mainMenuUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
