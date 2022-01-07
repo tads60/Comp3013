96affae9d27628b5c8453573623f905d6e2ffc5c
tads60 <philakers@live.co.uk>
2022-01-07 11:58:34 +0000

    Many
    Added light, prefabs, movement changes

--- Comp3013GraphicalPrototype/Assets/Scripts/BasicCharMove.cs
+++ Comp3013GraphicalPrototype/Assets/Scripts/BasicCharMove.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class BasicCharMove : MonoBehaviour
 {
+    [SerializeField] bool isfalling;
+    [SerializeField] bool hasDoubleJumped;
+    [SerializeField] bool canDoubleJump;
     private Vector3 tempPos;
     public GameObject character;
     public float movementModifier;
     public float jumpModifier;
-    public int health;
+    public int health = 4;
+    public int score;
+    public Rigidbody2D characterBody;
+    public float fallThresh = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,47 +25,88 @@ public class BasicCharMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        fallingCheck();
         if (Input.GetButton("Right"))
         {
-
-            tempPos = character.transform.position;
-
-            Vector3 newPos;
-            newPos.x = transform.position.x + movementModifier;
-            newPos.y = transform.position.y;
-            newPos.z = transform.position.z;
-
-            character.transform.position = Vector2.Lerp(transform.position, newPos, 1f);
+            moveRight();
         }
         if (Input.GetButton("Left"))
         {
 
-            tempPos = character.transform.position;
-
-            Vector3 newPos;
-            newPos.x = transform.position.x - movementModifier;
-            newPos.y = transform.position.y;
-            newPos.z = transform.position.z;
-
-            character.transform.position = Vector2.Lerp(transform.position, newPos, 1f);
+            moveLeft();
         }
         if (Input.GetButtonDown("Jump"))
         {
-
-            tempPos = character.transform.position;
-            if (tempPos.y <  -2.8)
-            {
-                Vector3 newPos;
-                newPos.x = transform.position.x;
-                newPos.y = transform.position.y + jumpModifier;
-                newPos.z = transform.position.z;
-
-                character.transform.position = Vector2.Lerp(transform.position, newPos, 1f);
-            }
-            //jumpCode
+            jump();
         }
     }
 
+    void moveLeft()
+    {
+
+        tempPos = character.transform.position;
+
+        Vector3 newPos;
+        newPos.x = transform.position.x - movementModifier;
+        newPos.y = transform.position.y;
+        newPos.z = transform.position.z;
+
+        character.transform.position = Vector2.Lerp(transform.position, newPos, 1f);
+        character.transform.GetComponent<SpriteRenderer>().flipX = true;
+    }
+
+    void moveRight()
+    {
+
+        tempPos = character.transform.position;
+
+        Vector3 newPos;
+        newPos.x = transform.position.x + movementModifier;
+        newPos.y = transform.position.y;
+        newPos.z = transform.position.z;
+
+        character.transform.position = Vector2.Lerp(transform.position, newPos, 1f);
+        character.transform.GetComponent<SpriteRenderer>().flipX = false;
+    }
+    void jump()
+    {
+        if (!isfalling)
+        {
+            //Vector3 newPos;
+            //newPos.x = transform.position.x;
+            //newPos.y = transform.position.y + jumpModifier;
+            //newPos.z = transform.position.z;
+
+            //character.transform.position = Vector2.Lerp(transform.position, newPos, 1f);
+            //jumpCode
+
+            characterBody.velocity = new Vector3(0, jumpModifier, 0);
+        }
+        else if (isfalling && !hasDoubleJumped && canDoubleJump)
+        {
+            //Vector3 newPos;
+            //newPos.x = transform.position.x;
+            //newPos.y = transform.position.y + jumpModifier;
+            //newPos.z = transform.position.z;
+
+            //character.transform.position = Vector2.Lerp(transform.position, newPos, 1f);
+            characterBody.velocity = new Vector3(0, jumpModifier, 0);
+            hasDoubleJumped = true;
+        }
+    }
+    void fallingCheck()
+    {
+        if(characterBody.velocity.y != fallThresh)
+        {
+            isfalling = true;
+        }
+        else
+        {
+            isfalling = false;
+            hasDoubleJumped = false;
+        }
+
+    }
     void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.tag == "Enemy")
@@ -73,5 +120,32 @@ public class BasicCharMove : MonoBehaviour
         {
             col.gameObject.transform.parent.GetComponent<EnemyMovement>().dead = true;
         }
+
+        if (col.gameObject.tag == "LethalHazard")
+        {
+            health = 0;
+        }
+
+        if (col.gameObject.tag == "Hazard")
+        {
+            health -= 1;
+        }
+
+        if (col.gameObject.tag == "Coin")
+        {
+            score += 1;
+            col.gameObject.SetActive(false);
+        }
+        //if (col.gameObject.tag == "Steps")
+        //{
+          //  movementModifier = movementModifier / 2;
+        //}
     }
+    //void OnCollisionExit2D(Collision2D col)
+    //{
+      //  if (col.gameObject.tag == "Steps")
+        //{
+          //  movementModifier = movementModifier * 2;
+       // }
+    //}
 }
--- Comp3013GraphicalPrototype/Assets/Scripts/EnemyMovement.cs
+++ Comp3013GraphicalPrototype/Assets/Scripts/EnemyMovement.cs
@@ -9,7 +9,11 @@ public class EnemyMovement : MonoBehaviour
     public GameObject enemy;
     public float movementModifier;
     public bool isLeft = true;
+    public bool isActive = false;
     public bool dead = false;
+    public GameObject player;
+    [SerializeField] float playerCloseness;
+    public float closenessFactor;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +24,48 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        closeCheck();
+
+        if (isActive)
+        {
+            if (isLeft)
+            {
+                left();
+            }
+            else if (!isLeft)
+            {
+                right();
+            }
+        }
+
+
+        if (dead)
+        {
+            Destroy(this.gameObject); 
+        }
+    }
+
+    void closeCheck()
+    {
+        playerCloseness = player.transform.position.x - enemy.transform.position.x;
+        if (playerCloseness < closenessFactor && playerCloseness > 0)
+        {
+            isActive = true;
+            isLeft = false;
+        }
+        else if (playerCloseness < 0 && playerCloseness > -closenessFactor)
+        {
+            isActive = true;
+            isLeft = true;
+        }
+        else
+        {
+            isActive = false;
+        }
+    }
+
+    void left()
+    {
         tempPos = enemy.transform.position;
         Vector3 newPos;
         newPos.x = transform.position.x - movementModifier;
@@ -27,9 +73,18 @@ public class EnemyMovement : MonoBehaviour
         newPos.z = transform.position.z;
 
         enemy.transform.position = Vector2.Lerp(transform.position, newPos, 1f);
-        if (dead)
-        {
-            Destroy(this.gameObject);
-        }
+        enemy.transform.GetComponent<SpriteRenderer>().flipX = false;
+    }
+
+    void right()
+    {
+        tempPos = enemy.transform.position;
+        Vector3 newPos;
+        newPos.x = transform.position.x + movementModifier;
+        newPos.y = transform.position.y;
+        newPos.z = transform.position.z;
+
+        enemy.transform.position = Vector2.Lerp(transform.position, newPos, 1f);
+        enemy.transform.GetComponent<SpriteRenderer>().flipX = true;
     }
 }
